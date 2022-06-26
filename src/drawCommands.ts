ec08f2de86ddf127e82a7dc9518135a10f097529
Vladimir Bessonov <bessmy@gmail.com>
2022-06-26 22:50:13 +0400

    feat: add draw commands

---
+++ src/drawCommands.ts
@@ -0,0 +1,47 @@
+import robot from 'robotjs';
+
+export const runDrawCommand = (command: string, args: string[]) => {
+  const { x, y } = robot.getMousePos();
+  const smoothSpeed = 100;
+
+  switch (command) {
+    case 'draw_circle':
+      robot.mouseClick('left');
+      robot.mouseToggle('down', 'left');
+      robot.setMouseDelay(5);
+
+      for (let i = 0; i <= Math.PI * 2; i += 0.01) {
+        const curX = x + Number(args[0]) * Math.cos(i) - Number(args[0]);
+        const curY = y + Number(args[0]) * Math.sin(i);
+        robot.dragMouse(curX, curY);
+      }
+
+      robot.mouseToggle('up');
+      break;
+    case 'draw_rectangle':
+      const curX = x + Number(args[0]);
+      const curY = y + Number(args[1]);
+      robot.mouseClick('left');
+      robot.mouseToggle('down', 'left');
+
+      robot.moveMouseSmooth(curX, y, smoothSpeed);
+      robot.moveMouseSmooth(curX, curY, smoothSpeed);
+      robot.moveMouseSmooth(x, curY, smoothSpeed);
+      robot.moveMouseSmooth(x, y, smoothSpeed);
+      robot.mouseToggle('up');
+      break;
+    case 'draw_square':
+      const rectX = x + Number(args[0]);
+      const rectY = y + Number(args[0]);
+      robot.mouseClick('left');
+      robot.mouseToggle('down', 'left');
+      robot.moveMouseSmooth(rectX, y, smoothSpeed);
+      robot.moveMouseSmooth(rectX, rectY, smoothSpeed);
+      robot.moveMouseSmooth(x, rectY, smoothSpeed);
+      robot.moveMouseSmooth(x, y, smoothSpeed);
+      robot.mouseToggle('up');
+      break;
+    default:
+      return;
+  }
+};
